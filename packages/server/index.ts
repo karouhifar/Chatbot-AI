0e0174a3f1a5812f8c56bedfe460399a9be1dbd2
karouhifar <karouhifar@gmail.com>
2025-11-22 15:02:24 -0500

    initial commit

---
+++ packages/server/index.ts
@@ -0,0 +1,22 @@
+import express from "express";
+import os from "os";
+
+const app = express();
+const PORT = process.env.PORT || 3000;
+const getNetworkAddress = () =>
+  Object.values(os.networkInterfaces())
+    .flat()
+    .find((i) => i?.family === "IPv4" && !i.internal)?.address || "localhost";
+
+console.log(process.env.CODE);
+
+const networkAddress = getNetworkAddress();
+app.use(express.json());
+
+app.get("/", (req, res) => {
+  res.json({ message: "Hello, World" });
+});
+
+app.listen(PORT, () => {
+  console.log(`Server is running on http://${networkAddress}:${PORT}`);
+});
